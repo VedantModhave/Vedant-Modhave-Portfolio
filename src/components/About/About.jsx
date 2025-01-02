1c4a9354e1ffd479698e608259d8dac957246eaf
Vedant Modhave <121169440+VedantModhave@users.noreply.github.com>
2025-01-03 00:29:31 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,3 +1,45 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/about.gif")} 
+          alt="Animated GIF showcasing my skills"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Web Developer</h3>
+              <p>
+                I'm a web developer with experience in building responsive platforms using technologies like HTML, CSS, React.js, and Firebase, focused on optimizing user experience and functionality.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/designer.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Designer</h3>
+              <p>
+                I'm a designer experienced in crafting intuitive UIs and creating visuals using tools like Canva, including posters and graphics for GDG TSEC.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+
+
 // import React from "react";
 
 // import styles from "./About.module.css";
@@ -38,43 +80,5 @@
 //   );
 // };
 
-import React from "react";
 
-import styles from "./About.module.css";
-import { getImageUrl } from "../../utils";
-
-export const About = () => {
-  return (
-    <section className={styles.container} id="about">
-      <h2 className={styles.title}>About</h2>
-      <div className={styles.content}>
-        <img
-          src={getImageUrl("about/about.gif")} 
-          alt="Animated GIF showcasing my skills"
-          className={styles.aboutImage}
-        />
-        <ul className={styles.aboutItems}>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Web Developer</h3>
-              <p>
-                I'm a web developer with experience in building responsive platforms using technologies like HTML, CSS, React.js, and Firebase, focused on optimizing user experience and functionality.
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/designer.png")} alt="UI icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Designer</h3>
-              <p>
-                I'm a designer experienced in crafting intuitive UIs and creating visuals using tools like Canva, including posters and graphics for GDG TSEC.
-              </p>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </section>
-  );
-};
 
