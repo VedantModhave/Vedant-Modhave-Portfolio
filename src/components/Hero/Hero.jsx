37e550036fdfaff3a1f00c164b9892cb329e19a6
Vedant Modhave <121169440+VedantModhave@users.noreply.github.com>
2025-06-14 22:29:40 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -17,8 +16,9 @@ export const Hero = () => {
             Let's Connect!
           </a>
           <a
-            href="/assets/Resume-Vedant Modhave.pdf"
-            download
+            href="https://drive.google.com/file/d/18Zraurnoejrke1iKs8DaK7gCBlfe1HRr/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
             className={styles.downloadBtn}
           >
             Download CV
@@ -35,35 +35,3 @@ export const Hero = () => {
     </section>
   );
 };
-
-
-
-
-
-// import React from "react";
-
-// import styles from "./Hero.module.css";
-// import { getImageUrl } from "../../utils";
-
-// export const Hero = () => {
-//   return (
-//     <section className={styles.container}>
-//       <div className={styles.content}>
-//         <h1 className={styles.title}>Hi, I'm Vedant Modhave</h1>
-//         <p className={styles.description}>
-//         A Web Developer with experience in building responsive platforms, focused on optimizing user experience and functionality.
-//         </p>
-//         <a href="#contact" className={styles.contactBtn}>
-//           Let's Connect!
-//         </a>
-//       </div>
-//       <img
-//         src={getImageUrl("hero/heroImage.png")}
-//         alt="Hero image of me"
-//         className={styles.heroImg}
-//       />
-//       <div className={styles.topBlur} />
-//       <div className={styles.bottomBlur} />
-//     </section>
-//   );
-// };
